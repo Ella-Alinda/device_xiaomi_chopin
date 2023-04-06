c959b0493e5209a22d7d7917f62c06f33724e2cb
HanCFCat <han.cf@foxmail.com>
2023-04-06 23:14:34 +0800

    chopin:Import xiaomi parts for android 11
    co-author:Joey <joey@lineageos.org>
    co-author:TheScarastic <warabhishek@gmail.com>
    co-author:ZeeLog <Danjel.Zammit@gmail.com>
    co-author:Luca Stefani <luca.stefani.ge1@gmail.com>
    co-author:Henrique Silva <jhenrique09.mcz@hotmail.com>
    co-author:Harsh Shandilya <msfjarvis@gmail.com>
    co-author:HarukeyUA <inazarrusnak@gmail.com>
    co-author:Akash <akashniki@gmail.com>
    co-author:darkbeast69 <guptaaryan189@gmail.com>
    co-author:Bruno Martins <bgcngm@gmail.com>
    co-author:Anderson Mendes <anderson2000ano@gmail.com>
    co-author:Roker2 <dimaminko2000@gmail.com>
    co-author:ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    co-author:Kingsman44 <shivan.0972@gmail.com>
    co-author:wbs306 <wbs306@gmail.com>
    co-author:Lostark13 <tushar9915@gmail.com>
    co-author:chrisw444 <wandersonrodriguesf1@gmail.com>
    co-author:Adithya R <gh0strider.2k18.reborn@gmail.com>
    co-author:WSTxda <wstxda@gmail.com>
    co-author:nullxception <enuma.alrizky@gmail.com>

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Xiaomi Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
