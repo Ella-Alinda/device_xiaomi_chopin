e5066937002e6fcb279faefee6945eb9610593a6
Ella Alinda <admin@ilolicon.com>
2023-07-17 14:59:21 +0800

    更新 'AndroidProducts.mk'

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,7 +17,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/exthm_chopin.mk \
-    $(LOCAL_DIR)/exthm_choping.mk
+    $(LOCAL_DIR)/exthm_choping.mk \
+    $(LOCAL_DIR)/aosp_chopin.mk
 
 COMMON_LUNCH_CHOICES := \
     exthm_chopin-user \
@@ -25,4 +26,7 @@ COMMON_LUNCH_CHOICES := \
     exthm_chopin-eng \
     exthm_choping-user \
     exthm_choping-userdebug \
-    exthm_choping-eng
+    exthm_choping-eng \
+    aosp_chopin-user \
+    aosp_chopin-userdebug \
+    aosp_chopin-eng
