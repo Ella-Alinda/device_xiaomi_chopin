7ba72029af139e2e71bfad298d0387d8f867a3fa
Ella Alinda <admin@ilolicon.com>
2024-01-01 16:04:36 +0800

    vibrator: Use aw8697 vibrator node instead

--- vibrator/include/vibrator-impl/Vibrator.h
+++ vibrator/include/vibrator-impl/Vibrator.h
@@ -24,9 +24,9 @@ namespace hardware {
 namespace vibrator {
 
 // Driver Nodes
-static constexpr char activate_node[] = "/sys/devices/platform/aw8622/activate";
-static constexpr char duration_node[] = "/sys/devices/platform/aw8622/duration";
-static constexpr char index_node[] = "/sys/devices/platform/aw8622/index";
+static constexpr char activate_node[] = "/sys/devices/platform/aw8697/activate";
+static constexpr char duration_node[] = "/sys/devices/platform/aw8697/duration";
+static constexpr char index_node[] = "/sys/devices/platform/aw8697/index";
 
 // Define durations for waveforms
 static constexpr uint32_t WAVEFORM_TICK_EFFECT_MS = 10;
